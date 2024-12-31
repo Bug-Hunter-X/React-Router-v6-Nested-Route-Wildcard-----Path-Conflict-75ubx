76ab8e61ce074ddbb49ac4d83942c1b85e5114a3
BugHunter-X <askzenai@gmail.com>
2024-12-31 07:28:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route will cause the error if nested inside a parent route with a wildcard (*) path. */}
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {return <div>Home</div>}
+function About() {return <div>About</div>}
+function Contact() {return <div>Contact</div>}
+export default App;
